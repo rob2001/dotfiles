78e588374ce36b7459699f0be6739ebae94b9e4c
Robert Smith <smithrobertlawrence@gmail.com>
2017-03-12 23:03:04 -0400

    added i3 configuration

---
+++ i3/.i3/locker.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+exec xautolock -detectsleep \
+  -time 3 -locker "i3lock -d -c 1453AD" \
+  -notify 30 \
+  -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'"
