00570e6f68005026076318e63fcc1229643b3d63
Robert Smith <smithrobertlawrence@gmail.com>
2017-03-20 23:57:06 -0400

    changed i3 directory structure
    switched to .config/i3/ from .i3/

--- i3/.config/i3/locker.sh
+++ i3/.config/i3/locker.sh
@@ -2,6 +2,6 @@
 
 xautolock -exit
 exec xautolock -detectsleep \
-  -time 3 -locker "i3lock -d -c 1453AD" \
+  -time 6 -locker "i3lock -d -c 1453AD" \
   -notify 30 \
   -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'"
